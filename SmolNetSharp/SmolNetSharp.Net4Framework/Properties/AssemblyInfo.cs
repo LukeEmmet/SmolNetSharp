d37cae0586811e9e1ec411d5ecee93c584c2ed27
Luke Emmet <luke.emmet@gmail.com>
2021-05-08 14:01:41 +0100

    fix project name

--- SmolNetSharp/SmolNetSharp.Net4Framework/Properties/AssemblyInfo.cs
+++ SmolNetSharp/SmolNetSharp.Net4Framework/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SmolNetSharpFramework")]
+[assembly: AssemblyTitle("SmolNetSharp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SmolNetSharpFramework")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyProduct("SmolNetSharp")]
+[assembly: AssemblyCopyright("Copyright © MarmaladeFoo software 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
